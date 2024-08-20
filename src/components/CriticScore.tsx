10dcd0b990a4b7b2e3eb282a8ac8796423d41f1a
Pham Huy Anh <phamhuyanh277@gmail.com>
2024-08-20 23:20:43 +0300

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "red";
+
+  return (
+    <div>
+      <Badge
+        colorScheme={color}
+        fontSize={"14px"}
+        paddingX={2}
+        borderRadius={4}
+      >
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default CriticScore;
